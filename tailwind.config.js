0edc4725ba5c2954b1f4292907dcac61e2a29d47
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-03-01 23:22:24 +0330

    add customize styles in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,54 @@
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
-  theme: {
-        extend: {},
+    content: ['./public/**/*.{html,js}'],
+    theme: {
+        fontFamily: {
+            body: ['IRANSans', 'iranyekan'],
+            display: ['IRANSans', 'iranyekan'],
+        },
+
+        screens: {
+            xs: '475px',
+            // => @media (min-width: 475px) { ... }
+
+            sm: '640px',
+            // => @media (min-width: 640px) { ... }
+
+            md: '825px',
+            // => @media (min-width: 840px) { ... }
+
+            lg: '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            xl: '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+        },
+
+        extend: {
+            colors: {
+                green: {
+                    snapp: '#22aa58',
+                    snappBtn: '#00d170',
+                },
+            },
+            borderWidth: {
+                1: '1px',
+            },
+            width: {
+                80: '20rem',
+                84: '21rem',
+                88: '22rem',
+                92: '23rem',
+            },
+            lineHeight: {
+                12: '3rem',
+                16: '4rem',
+            },
+            letterSpacing: {
+                tightest: '-.01em',
+            },
+        },
     },
     plugins: [],
 };

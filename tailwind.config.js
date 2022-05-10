c773f7d234dde3aef649ada2a4fba193e812bea3
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-11 00:20:24 +0430

    add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,55 +1,56 @@
 module.exports = {
-    content: ['./public/**/*.{html,js}'],
-    theme: {
-        fontFamily: {
-            body: ['IRANSans', 'iranyekan'],
-            display: ['IRANSans', 'iranyekan'],
-        },
+	content: ['./public/**/*.{html,js}'],
+	theme: {
+		fontFamily: {
+			body: ['IRANSans', 'iranyekan'],
+			display: ['IRANSans', 'iranyekan'],
+		},
 
-        screens: {
-            xs: '475px',
-            // => @media (min-width: 475px) { ... }
+		screens: {
+			xs: '475px',
+			// => @media (min-width: 475px) { ... }
 
-            sm: '640px',
-            // => @media (min-width: 640px) { ... }
+			sm: '640px',
+			// => @media (min-width: 640px) { ... }
 
-            md: '825px',
-            // => @media (min-width: 840px) { ... }
+			md: '825px',
+			// => @media (min-width: 840px) { ... }
 
-            lg: '1024px',
-            // => @media (min-width: 1024px) { ... }
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
 
-            xl: '1280px',
-            // => @media (min-width: 1280px) { ... }
+			xl: '1280px',
+			// => @media (min-width: 1280px) { ... }
 
-            '2xl': '1536px',
-        },
+			'2xl': '1536px',
+		},
 
-        extend: {
-            colors: {
-                green: {
-                    snapp: '#22aa58',
-                    snappBtn: '#00d170',
-                },
-            },
-            borderWidth: {
-                1: '1px',
-            },
-            width: {
-                54: '13.5rem',
-                80: '20rem',
-                84: '21rem',
-                88: '22rem',
-                92: '23rem',
-            },
-            lineHeight: {
-                12: '3rem',
-                16: '4rem',
-            },
-            letterSpacing: {
-                tightest: '-.01em',
-            },
-        },
-    },
-    plugins: [],
+		extend: {
+			colors: {
+				green: {
+					snapp: '#22aa58',
+					snappBtn: '#00d170',
+				},
+			},
+			borderWidth: {
+				1: '1px',
+			},
+			width: {
+				30: '7.5rem',
+				54: '13.5rem',
+				80: '20rem',
+				84: '21rem',
+				88: '22rem',
+				92: '23rem',
+			},
+			lineHeight: {
+				12: '3rem',
+				16: '4rem',
+			},
+			letterSpacing: {
+				tightest: '-.01em',
+			},
+		},
+	},
+	plugins: [],
 };
